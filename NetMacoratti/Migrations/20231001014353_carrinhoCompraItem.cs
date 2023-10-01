4e3a03024227039a5160fba09df150d2c80e609d
Lindemberg Rodrigues <lindemberg.barbosa@performait.com>
2023-10-01 15:24:06 -0300

    Migrations para o carrinho de compras

---
+++ NetMacoratti/Migrations/20231001014353_carrinhoCompraItem.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetMacoratti.Migrations
+{
+    /// <inheritdoc />
+    public partial class carrinhoCompraItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrinho",
+                columns: table => new
+                {
+                    CarrinhoCompraItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LancheId = table.Column<int>(type: "int", nullable: true),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    CarrinhoCompraId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrinho", x => x.CarrinhoCompraItemId);
+                    table.ForeignKey(
+                        name: "FK_Carrinho_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrinho_LancheId",
+                table: "Carrinho",
+                column: "LancheId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carrinho");
+        }
+    }
+}
