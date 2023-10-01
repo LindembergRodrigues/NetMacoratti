0edbddb412cd656b42a28233dd84b19ab102c3e9
Lindemberg Rodrigues <lindemberg.barbosa@performait.com>
2023-10-01 15:26:38 -0300

    ajustes da view e logica de lanches

---
+++ NetMacoratti/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using NetMacoratti.Models;
+
+namespace NetMacoratti.ViewModels
+{
+    public class LancheListViewModel
+    {
+
+        public IEnumerable<Lanche> lanches { get; set; }
+
+        public string  categoriaAtual { get; set; }
+    }
+}
