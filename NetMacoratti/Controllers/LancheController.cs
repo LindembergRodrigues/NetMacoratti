7ec4aca14b007f7b3af1fa7154e6e81dbdaa8fc8
Lindemberg Rodrigues <lindemberg.barbosa@performait.com>
2023-09-30 21:17:16 -0300

    Controlador de lanches

--- NetMacoratti/Controllers/LancheController.cs
+++ NetMacoratti/Controllers/LancheController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetMacoratti.Repository.Interface;
 
 namespace NetMacoratti.Controllers
 {
     public class LancheController : Controller
     {
+
+       public readonly ILancheRepository lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            this.lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult List()
+        {
+            var lanches = lancheRepository.Lanches;
+            return View(lanches);
+        }
+
     }
 }
