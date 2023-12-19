f12484c8fe00c81bda5180a3840089462e91eeb7
Lindemberg <bergfla2010@hotmail.com>
2023-12-19 20:35:41 -0300

    ajustes

---
+++ NetMacoratti/Component/CarrinhoCompraResumo.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using NetMacoratti.Models;
+using NetMacoratti.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetMacoratti.Component
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra CarrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            this.CarrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //CarrinhoCompra.CarrinhoCompraItens = CarrinhoCompra.GetCarrinhoCompraItens();
+
+            CarrinhoCompra.CarrinhoCompraItens = new List<CarrinhoCompraItem>() { new CarrinhoCompraItem(), new CarrinhoCompraItem() };
+
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel()
+            {
+                carrinhoCompra = CarrinhoCompra,
+                CarrinhoCompraTotal = CarrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+    }
+}
